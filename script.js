6f0b617df1fe1430483b124b553b9962f62aa83f
UT0911 <giantprogrampanda33@gmail.com>
2024-06-05 20:47:56 +0900

    script.jsを追加

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(function () {
+    function end_loader() {
+      $('.loader').fadeOut(800);
+    }
+    $(window).on('load', function () {
+      setTimeout(function () {
+        end_loader();
+      }, 3000)
+    })
+  })
+  
